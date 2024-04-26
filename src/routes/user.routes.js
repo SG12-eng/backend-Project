871c52c9ae74a71bfe135cd57f22064ed44975f3
Sagar Gupta <guptasagar694@gmail.com>
2024-04-26 18:38:38 +0530

    RefreshToken , AccessToken and Middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router } from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { loginUSer, logoutUser, registerUser } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router()
 
@@ -16,6 +17,11 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUSer)
+
+// Secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 
 export default router
