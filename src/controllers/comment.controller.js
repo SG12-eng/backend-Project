e3997410963b052dc14051831a9f8309de585e07
Sagar Gupta <guptasagar694@gmail.com>
2024-05-30 15:51:00 +0530

    Subscription controller added

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -6,18 +6,69 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  const options = {
+    page:  parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  }
+
+  const aggregate = Comment.aggregate([
+    { $match : {video: mongoose.Types.ObjectId(videoId)}},
+    { $sort: {createdAt: -1}},
+  ])
+
+  const comments = await Comment.aggregatePaginate(aggregate, options);
+
+  res.json(new ApiResponse(200, "Comments retrieved", comments))
+
 });
 
 const addComment = asyncHandler(async (req, res) => {
     //add a comment to a video
+    const {videoId} = req.params;
+
+    const {content, ownerId } = req.body;
+
+    const addComment = new Comment({content, video: videoId, owner: ownerId});
+
+    await addComment.save();
+
+    res.json(new ApiResponse(201, addComment, "Comment added"))
+
 })
 
 const updateComment = asyncHandler(async(req, res) => {
     // update a comment
+    const {commentId} = req.params;
+
+    const {content} = req.body;
+
+    const comment = await Comment.findById(commentId);
+    if(!comment) {
+      throw new ApiError(404, "comment not found")
+    }
+
+    comment.content  = content;
+    await content.save();
+
+    res.json(new ApiResponse(200, comment, "Comment Updated"))
+
 })
 
 const deleteComment = asyncHandler(async(req, res) => {
-    // delete a comment
+  const {commentId} = req.params;
+  const {content} = req.body;
+
+  const comment = await Comment.findBy
+  if(!comment) {
+    throw new ApiError(404, "comment not found")
+  }
+
+  comment.content = content;
+  await content.remove()
+
+  res.json(new ApiResponse(200, "Comment deleted"))
+  
 })
 
 export {
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,16 +8,70 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const  toggleSubscription = asyncHandler( async(req, res) => {
     const {channelId} = req.params
     // toggle subscription
+    const subscriberId = req.user._id;
+
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Inavlid Channel Id")
+    }
+
+    const channel = await User.findById(channelId);
+    if(!channel) {
+        throw new ApiError(404, "channel not found")
+    }
+
+    const existingSubscription = await Subscription.findOne({
+        channel: channelId, subscriber: subscriberId
+    })
+
+    if(existingSubscription) {
+        await Subscription.deleteOne({_id: existingSubscription._id})
+    } else{
+        const newSubscription = new Subscription({ channel: channelId, subscriber: subscriberId});
+        await newSubscription.save();
+        return res.json(new ApiResponse(201, "Subscribed successfully"))
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async(req, res) => {
-    const {channelId} = req.params
+    const {channelId} = req.params;
+
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "invalid Channel id")
+    }
+
+   const channel = await User.findById(channelId)
+   if(!channel){
+    throw new ApiError(404, "Channel doesnot exist")
+   }
+
+   const subscriberList =  await Subscription.find({channel: channelId}).populate("subscriber", "fullname")
+
+   const subscribers = subscriberList.map(subscription => subscription.subscriber)
+
+   res.json(new ApiResponse(200, subscribers , "Total subscribers"))
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler( async(req, res) => {
-    const { subscriberId } = req.params 
+    const { subscriberId } = req.params;
+
+    if(!isValidObjectId(subscriberId)){
+        throw new ApiError(400, "Inavlid subscriber Id")
+    }
+
+    const subscriber = await User.findById(subscriberId);
+    if(!subscriber) {
+        throw new ApiError(404, "subscriber not found")
+    }
+
+    const channels = await Subscription.find({subscriber : subscriberId}).populate("channel","username")
+
+    const channelsSubscribed = channels.map(subscription => subscription.channel);
+
+    res.json(new ApiResponse(200, "Subscribed Channels", channelsSubscribed))
+
 })
 
 export {
